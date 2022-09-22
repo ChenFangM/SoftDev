b84d11cf4437bb7dd32cfbf3606ddccd7595285c
ChenFangM <Fang1113848389@gmail.com>
2022-09-22 09:16:08 -0400

    krewes

---
+++ 04_choose/krewes.py
@@ -0,0 +1,14 @@
+"""
+JFCCC: Fang Chen, Jeff Chen
+Soft Dev, pd 2
+
+DISCO:
+
+QCC:
+
+OPS Summary:
+
+"""
+
+krewes = {2:["Fang", "Jeff"], 7:["7Devo1", "7Devo2"], 8:["8Devo1", "8Devo2"]}
+print((krewes))
