47a8975c90337d4770198c715896932faf8c53ac
ChenFangM <Fang1113848389@gmail.com>
2022-09-22 09:29:44 -0400

    random.randint() qworkssss

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -3,12 +3,25 @@ JFCCC: Fang Chen, Jeff Chen
 Soft Dev, pd 2
 
 DISCO:
+Access dict items w/ dictName[key] or dictName.get(key())
 
 QCC:
+Why does...
+
+    from random import seed
+    from random import randint
+    for _ in range(2):
+        print(randint(0, 2))
+
+... only print 2 [newline] 0?
 
 OPS Summary:
 
 """
+import random
 
-krewes = {2:["Fang", "Jeff"], 7:["7Devo1", "7Devo2"], 8:["8Devo1", "8Devo2"]}
-print((krewes))
+krewes = {2:["Jeff", "Fang"], 7:["7Devo1", "7Devo2"], 8:["8Devo1", "8Devo2"]}
+print((krewes[2])[random.randint(0,2)])
+
+for _ in range(2):
+    print(random.randint(0, 2))
