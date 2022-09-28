dc4e50f8f4a2dbf71ee0376c2942b36ae3b16b5f
ChenFangM <Fang1113848389@gmail.com>
2022-09-28 09:27:32 -0400

    howdowedothis

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,15 @@
+"""
+Fang Chen, Julia Lee
+Soft Dev, pd 2
+
+DISCO:
+QCC:
+OPS Summary:
+
+"""
+import random
+
+period = random.choice(list(krewes.keys()))
+print(period)
+krewe = random.choice(krewes[period])
+print(krewe)
