d118c2d506c889682c8d49229421c67acb76f919
ChenFangM <Fang1113848389@gmail.com>
2022-09-29 09:30:29 -0400

    k5 done

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,6 +1,9 @@
 """
-Fang Chen, Julia Lee
+RANDO: Fang Chen, Julia Lee
 Soft Dev, pd 2
+K05 -- Krewes
+2022-09-28
+time spent: (classtime) 1.5 hrs
 
 DISCO:
 QCC:
@@ -9,7 +12,30 @@ OPS Summary:
 """
 import random
 
+"""
+krewes = {
+           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY"], 
+           7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+           8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "wanying"]
+         }
+"""
+
+krewes = {}
+
+file = open("krewes.txt")
+txt = file.read().strip()
+splitCorr = txt.split("@@@")
+
+for x in splitCorr:
+    t = x.split("$$$")     # t = [pd, name, ducky]
+    if int(t[0]) not in list(krewes.keys()):
+        krewes[int(t[0])] = [t[1:]]
+    else:
+        krewes[int(t[0])].append(t[1:])
+    
+
 period = random.choice(list(krewes.keys()))
 print(period)
 krewe = random.choice(krewes[period])
 print(krewe)
+print(krewes)
