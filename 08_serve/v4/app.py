e9f3c1756ad3d0cf34be0794ae56f38be05c37fe
ChenFangM <ChenFang.NY@gmail.com>
2022-10-07 01:33:15 -0400

    Pull before editing!

--- 08_serve/v4/app.py
+++ 08_serve/v4/app.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 # New Deal: Fang Min Chen, Ryan Lau, Anson Wong
 # SoftDev
 # K08: Serve
 # Oct 2022
 # time spent: .2 hrs
-=======
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2022
->>>>>>> 3a067dcd7ead1dca8da0cf3b25e451cdecffe358
 
 from flask import Flask
 app = Flask(__name__) #create instance of class Flask
@@ -22,9 +16,6 @@ def hello_world():
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
-<<<<<<< HEAD
 
 # We predict that if __name__ is not __main__, then nothing will happen
 # QCC: When is __name__ not __main__?
-=======
->>>>>>> 3a067dcd7ead1dca8da0cf3b25e451cdecffe358
