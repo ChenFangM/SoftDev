ebbdc498c42f668fb445e9fd0b537eb1f8610505
ChenFangM <Fang1113848389@gmail.com>
2022-11-03 09:29:48 -0400

    cw

---
+++ 19_session/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, session
+
+app = Flask(__name__)
+
+app.secret_key = b'dusil239kds[#$2gf@'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
